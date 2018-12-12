3d5c9113e36726d0cda9759c6da1266bdd96425e
svmihar <svmihar@gmail.com>
2018-12-13 00:01:29 +0700

    add random forest, tapi masih kurang karena hanya bikin plotting belum decision tree nya

---
+++ temp/randomforest.py
@@ -0,0 +1,54 @@
+#Import scikit-learn dataset library
+from sklearn import datasets
+
+#Load dataset
+iris = datasets.load_iris()
+
+# print the label species(setosa, versicolor,virginica)
+print(iris.target_names)
+
+# print the names of the four features
+print(iris.feature_names)
+
+# print the iris data (top 5 records)
+print(iris.data[0:5])
+
+# print the iris labels (0:setosa, 1:versicolor, 2:virginica)
+print(iris.target)
+
+# Creating a DataFrame of given iris dataset.
+import pandas as pd
+data=pd.DataFrame({
+    'sepal length':iris.data[:,0],
+    'sepal width':iris.data[:,1],
+    'petal length':iris.data[:,2],
+    'petal width':iris.data[:,3],
+    'species':iris.target
+})
+print(data.head())
+
+# Import train_test_split function
+from sklearn.model_selection import train_test_split
+
+X=data[['sepal length', 'sepal width', 'petal length', 'petal width']]  # Features
+y=data['species']  # Labels
+
+# Split dataset into training set and test set
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2) #split 80 for train 20 for testing
+
+#Import Random Forest Model
+from sklearn.ensemble import RandomForestClassifier
+
+#Create a Gaussian Classifier
+clf=RandomForestClassifier(n_estimators=100)
+
+#Train the model using the training sets y_pred=clf.predict(X_test)
+clf.fit(X_train,y_train)
+
+y_pred=clf.predict(X_test)
+print(y_pred)
+from sklearn import metrics
+# Model Accuracy, how often is the classifier correct?
+print("Accuracy:",metrics.accuracy_score(y_test, y_pred))
+predict_ = clf.predict([[2, 1, 6, 1]])
+
